d7d32a38b2c468ab7f1ba2ce5724585e77074410
Maxim Kirshin <meowto16@gmail.com>
2022-08-26 22:49:19 +0500

    Challenge: Temperature (Part 2)

--- challenge/temperature.cpp
+++ challenge/temperature.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
 int main() {
-    double tempf = 72;
+
+    double tempf;
     double tempc;
+
+    std::cout << "Enter the temperature in Fahrenheit:";
+
+    std::cin >> tempf;
 
     tempc = (tempf - 32) / 1.8;
 
     std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+
 }
