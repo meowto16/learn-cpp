86215e48d0aebd65843cef8ff15338b597f306e6
Maxim Kirshin <meowto16@gmail.com>
2022-09-05 11:54:05 +0500

    Lesson 9. Increment and decrement. Completed

---
+++ lessons/009-increment-decrement/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 0;
+  int b = 0;
+
+  cout << "a++: " << a++ << endl;
+  cout << "++b: " << ++b << endl;
+
+  cout << "a--: " << a-- << endl;
+  cout << "--b: " << --b << endl;
+}
