089ecad29aa311e24cdeefa1f983ec21c9139e85
Maxim Kirshin <meowto16@gmail.com>
2022-09-05 21:35:10 +0500

    Lesson 10. Short arithmetic operators. Completed

---
+++ lessons/010-short-arithmetic-operators/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 5;
+  int b = 10;
+  int c = 15;
+  int d = 20;
+  int e = 30;
+
+  a += 5;
+
+  b -= 5;
+
+  c /= 5;
+
+  d *= 10;
+
+  e %= 5;
+
+  cout << a << endl;
+  cout << b << endl;
+  cout << c << endl;
+  cout << d << endl;
+  cout << e << endl;
+}
