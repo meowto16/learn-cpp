a195648f794e9c4e11e4e5b85739b442220774c2
Maxim Kirshin <meowto16@gmail.com>
2022-09-10 19:41:48 +0500

    Lesson 27. Output array. Completed

---
+++ lessons/027-output-array/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  const int size = 4;
+  int arr[size] = {9, 1, 4, 3};
+
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << endl;
+  }
+}
