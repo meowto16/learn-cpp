953668595b9a94928e6304c18665de04a514c53d
Maxim Kirshin <meowto16@gmail.com>
2022-09-12 00:36:21 +0500

    Lesson 32. Two dimensional array fill. Completed

---
+++ lessons/032-two-dimensional-arr-fill/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  const int ROWS = 5;
+  const int COLS = 8;
+
+  int arr[ROWS][COLS];
+
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      arr[i][j] = rand() % 10;
+      cout << arr[i][j] << '\t';
+    }
+
+    cout << endl;
+  }
+}
